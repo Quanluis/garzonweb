667baad2eb223f4eb7fcb239bab63e7b187a2507
Quanluis <quanluis@protonmail.com>
2019-01-21 21:36:16 -0500

    Created a simple navbar with state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import './app.css'
+import './App.css'
 
 class App extends Component{
   render() {
     
     this.State= {
       title: "Garzon Web",
-      
+      main: 'Home',
+      about: 'About me',
+      contact: 'Contact',
+      work: 'Portfolio',
+      link: 'www.youtube.com'
     }
 
     return (
      <div>  
+       <div>
+        <ul>
+          <ul>{this.State.main}</ul>
+          <ul>{this.State.about}</ul>
+          <ul>{this.State.contact}</ul>
+          <ul>{this.State.work}</ul>
+        </ul>
+       </div>
        <h1>{this.State.title}</h1>
      </div>
     
