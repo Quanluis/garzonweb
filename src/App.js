b2c3ad2d08545b7d26f2d34e3d9ebdaae4a70b4e
Quanluis <quanluis@protonmail.com>
2019-02-04 19:04:09 -0500

    created some simple functions for testing and fixed positioning issues with CSS

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React, { Component } from 'react';
 import './App.css'
 
 class App extends Component{
+
+  click = (e) =>  {
+    e.preventDefault()
+    console.log('This is: ', this.State.main)
+  }
+
+  onHover = () => {
+    
+  }
+  
+
   render() {
     
     this.State= {
       title: "Garzon Web",
       main: 'Home',
-      about: 'About me',
+      about: 'About',
       contact: 'Contact',
       work: 'Portfolio',
       link: 'www.youtube.com'
     }
 
     return (
+
+
+     
      <div>  
        <div>
         <ul>
-          <ul>{this.State.main}</ul>
-          <ul>{this.State.about}</ul>
-          <ul>{this.State.contact}</ul>
-          <ul>{this.State.work}</ul>
+          <ul><a onClick = {this.click} href="">{this.State.main}</a></ul>
+          <ul><a href="">{this.State.about}</a></ul>
+          <ul><a href="">{this.State.contact}</a></ul>
+          <ul><a href="">{this.State.work}</a></ul>
         </ul>
        </div>
        <h1>{this.State.title}</h1>
