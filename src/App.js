1357aad79a94d649e1fe79438a552b3a02105728
Quanluis <quanluis@protonmail.com>
2019-02-05 22:21:35 -0500

    added change color on hover

--- src/App.js
+++ src/App.js
@@ -9,16 +9,13 @@ class App extends Component{
     console.log('This is: ', this.State.main)
   }
 
-  
-  
-
   render() {
     
     this.State= {
       title: "Garzon Web",
       main: 'Home',
       about: 'About',
-      contact: 'Contact',
+      github: 'Github',
       work: 'Portfolio',
       link: 'www.youtube.com'
     }
@@ -30,9 +27,9 @@ class App extends Component{
      <div>  
        <div className = 'nav' >
         <ul>
-          <ul><a onClick = {this.click} href="">{this.State.main}</a></ul>
+          <ul><a onClick = {this.changeOnHover} target = "_blank"  href = { 'https://github.com/Quanluis' } >  {this.State.main}</a></ul>
           <ul><a href="">{this.State.about}</a></ul>
-          <ul><a href="">{this.State.contact}</a></ul>
+          <ul><a href="">{this.State.github}</a></ul>
           <ul><a href="">{this.State.work}</a></ul>
         </ul>
        </div>
